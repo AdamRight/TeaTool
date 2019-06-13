f7bef3770757b3ef1c52f25e201d11239cbeabb8
AdamRight <1131045891@qq.com>
2019-06-13 22:52:43 +0800

    自定义九宫格初始化实现

--- app/src/main/java/com/tea/teatool/mazelock/MazeLockView.java
+++ app/src/main/java/com/tea/teatool/mazelock/MazeLockView.java
@@ -5,7 +5,11 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Admin on 2019/6/10.
@@ -15,7 +19,7 @@ public class MazeLockView extends View {
 
     private boolean isInit = false;
 
-    private int mDotRadius = 0;
+    private float mDotRadius = 0;
 
     // 二维数组初始化，int[3][3]
     private Point[][] mPoints = new Point[3][3];
@@ -33,6 +37,13 @@ public class MazeLockView extends View {
     private int mInnerNormalColor = 0xff929292;
     private int mOuterErrorColor = 0xff901032;
     private int mInnerErrorColor = 0xffea0945;
+
+    private float mMovingX = 0f;
+    private float mMovingY = 0f;
+
+    private boolean isTouchPoint = false;
+
+    private List<Point> selectPoints = new ArrayList<>();
 
     public MazeLockView(Context context) {
         this(context, null);
@@ -60,14 +71,70 @@ public class MazeLockView extends View {
     private void drawMaze(Canvas canvas) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < mPoints[i].length; j++) {
-                // 先绘制外圆
-                mNormalPaint.setColor(mOuterNormalColor);
-                canvas.drawCircle(mPoints[i][j].x, mPoints[i][j].y, mDotRadius, mNormalPaint);
-                // 后绘制内圆
-                mNormalPaint.setColor(mInnerNormalColor);
-                canvas.drawCircle(mPoints[i][j].x, mPoints[i][j].y, mDotRadius / 6, mNormalPaint);
+
+                if (mPoints[i][j].statusIsPressed()) {
+                    // 先绘制外圆
+                    mPressedPaint.setColor(mOuterPressedColor);
+                    canvas.drawCircle(mPoints[i][j].x, mPoints[i][j].y, mDotRadius, mPressedPaint);
+                    // 后绘制内圆
+                    mPressedPaint.setColor(mInnerPressedColor);
+                    canvas.drawCircle(mPoints[i][j].x, mPoints[i][j].y, mDotRadius / 6, mPressedPaint);
+                }
+
+                if (mPoints[i][j].statusIsNormal()) {
+                    // 先绘制外圆
+                    mNormalPaint.setColor(mOuterNormalColor);
+                    canvas.drawCircle(mPoints[i][j].x, mPoints[i][j].y, mDotRadius, mNormalPaint);
+                    // 后绘制内圆
+                    mNormalPaint.setColor(mInnerNormalColor);
+                    canvas.drawCircle(mPoints[i][j].x, mPoints[i][j].y, mDotRadius / 6, mNormalPaint);
+                }
+
+                if (mPoints[i][j].statusIsError()) {
+                    // 先绘制外圆
+                    mErrorPaint.setColor(mOuterErrorColor);
+                    canvas.drawCircle(mPoints[i][j].x, mPoints[i][j].y, mDotRadius, mErrorPaint);
+                    // 后绘制内圆
+                    mErrorPaint.setColor(mInnerErrorColor);
+                    canvas.drawCircle(mPoints[i][j].x, mPoints[i][j].y, mDotRadius / 6, mErrorPaint);
+                }
             }
         }
+
+        drawLine(canvas);
+    }
+
+    private void drawLine(Canvas canvas) {
+        if (selectPoints.size() >= 1) {
+
+            Point lastPoint = selectPoints.get(0);
+
+            for (int i = 1; i < selectPoints.size() - 1; i++) {
+                drawLine(lastPoint, selectPoints.get(i), canvas, mLinePaint);
+
+                lastPoint = selectPoints.get(i);
+            }
+
+
+            boolean isInnerPoint = checkInRound(lastPoint.x, lastPoint.y, mDotRadius / 4, mMovingX, mMovingY);
+            if (!isInnerPoint && isTouchPoint) {
+                drawLine(lastPoint, new Point(mMovingX, mMovingY, -1), canvas, mLinePaint);
+            }
+        }
+    }
+
+    private void drawLine(Point start, Point end, Canvas canvas, Paint paint) {
+
+        double distance = distance(start.x, start.y, end.x, end.y);
+
+        float dx = end.x - start.x;
+        float dy = end.y - start.y;
+
+        float rx = (float) (dx / distance * (mDotRadius / 6.0));
+        float ry = (float) (dy / distance * (mDotRadius / 6.0));
+
+        canvas.drawLine(start.x + rx, start.y + ry, end.x - rx, end.y - ry, paint);
+
     }
 
     private void initPaint() {
@@ -111,19 +178,83 @@ public class MazeLockView extends View {
         mPoints[0][0] = new Point(spaceWidth / 2, offsetY + spaceWidth / 2, 0);
         mPoints[0][1] = new Point(spaceWidth * 3 / 2, offsetY + spaceWidth / 2, 1);
         mPoints[0][2] = new Point(spaceWidth * 5 / 2, offsetY + spaceWidth / 2, 2);
-        mPoints[1][0] = new Point(spaceWidth / 2, offsetY + spaceWidth * 3/ 2, 3);
-        mPoints[1][1] = new Point(spaceWidth * 3 / 2, offsetY + spaceWidth * 3/ 2, 4);
-        mPoints[1][2] = new Point(spaceWidth * 5 / 2, offsetY + spaceWidth * 3/ 2, 5);
-        mPoints[2][0] = new Point(spaceWidth / 2, offsetY + spaceWidth * 5/ 2, 6);
-        mPoints[2][1] = new Point(spaceWidth * 3 / 2, offsetY + spaceWidth * 5/ 2, 7);
-        mPoints[2][2] = new Point(spaceWidth * 5 / 2, offsetY + spaceWidth * 5/ 2, 8);
+        mPoints[1][0] = new Point(spaceWidth / 2, offsetY + spaceWidth * 3 / 2, 3);
+        mPoints[1][1] = new Point(spaceWidth * 3 / 2, offsetY + spaceWidth * 3 / 2, 4);
+        mPoints[1][2] = new Point(spaceWidth * 5 / 2, offsetY + spaceWidth * 3 / 2, 5);
+        mPoints[2][0] = new Point(spaceWidth / 2, offsetY + spaceWidth * 5 / 2, 6);
+        mPoints[2][1] = new Point(spaceWidth * 3 / 2, offsetY + spaceWidth * 5 / 2, 7);
+        mPoints[2][2] = new Point(spaceWidth * 5 / 2, offsetY + spaceWidth * 5 / 2, 8);
 
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mMovingX = event.getX();
+        mMovingY = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Point point = getPoint();
+                if (point != null) {
+                    isTouchPoint = true;
+                    selectPoints.add(point);
+
+                    point.setStatusPressed();
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (isTouchPoint) {
+                    Point pointMove = getPoint();
+                    if (pointMove != null && !selectPoints.contains(pointMove)) {
+                        selectPoints.add(pointMove);
+                        pointMove.setStatusPressed();
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                isTouchPoint = false;
+                break;
+
+        }
+
+        invalidate();
+        return true;
+    }
+
+    /**
+     * 获取按下的点
+     *
+     * @return 当前按下的点
+     */
+    private Point getPoint() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < mPoints.length; j++) {
+                // for 循环九个点，判断手指位置是否在这个九个点里面
+                if (checkInRound(mPoints[i][j].x, mPoints[i][j].y, mDotRadius, mMovingX, mMovingY)) {
+                    return mPoints[i][j];
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean checkInRound(float sx, float sy, float r, float x,
+                                       float y) {
+        // x的平方 + y的平方 开根号 < 半径
+        return Math.sqrt((sx - x) * (sx - x) + (sy - y) * (sy - y)) < r;
+    }
+
+    public static double distance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.abs(x1 - x2) * Math.abs(x1 - x2) + Math.abs(y1 - y2) * Math.abs(y1 - y2));
+    }
+
     class Point {
-        public int x, y,index;
-        public Point(int x, int y, int index) {
-            this.x = x ;
+
+        public float x;
+        public float y;
+        public int index;
+
+        public Point(float x, float y, int index) {
+            this.x = x;
             this.y = y;
             this.index = index;
         }
@@ -133,5 +264,29 @@ public class MazeLockView extends View {
         private int STATUS_ERROR = 3;
         //当前点的状态 有三种状态
         private int status = STATUS_NORMAL;
+
+        public void setStatusPressed() {
+            status = STATUS_PRESSED;
+        }
+
+        public void setStatusNormal() {
+            status = STATUS_NORMAL;
+        }
+
+        public void setStatusError() {
+            status = STATUS_ERROR;
+        }
+
+        public boolean statusIsPressed() {
+            return status == STATUS_PRESSED;
+        }
+
+        public boolean statusIsNormal() {
+            return status == STATUS_NORMAL;
+        }
+
+        public boolean statusIsError() {
+            return status == STATUS_ERROR;
+        }
     }
 }
