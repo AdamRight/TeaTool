0e54a012332ca53892fe1acdd673d2a823782bfa
AdamRight <1131045891@qq.com>
2019-12-18 21:16:07 +0800

    kotlin

--- app/src/main/java/com/tea/teatool/kotlin/KotlinTestActivity.kt
+++ app/src/main/java/com/tea/teatool/kotlin/KotlinTestActivity.kt
@@ -20,10 +20,69 @@ class KotlinTestActivity : AppCompatActivity() {
 
 
         var strArrays = arrayOf("1", "2", "", "", "3", "4")
-        strArrays.filter { it.isNotEmpty() }.forEach{ println(it)}
+        strArrays.filter { it.isNotEmpty() }.forEach { println(it) }
+
+        Outter@ for ((index, value) in strArrays.withIndex()) {
+            println("$index -> $value")
+            Inner@ for (indexedValue in strArrays.withIndex()) {
+                if (true) {
+                    break@Outter
+                }
+            }
+        }
+
+        val view = View()
+        view.onclick = object : OnClickListener {
+            override fun onClick() {
+            }
+        }
+
+
 
     }
 
 }
 
+class Woman {
+    var nameA: String = "翠花"
+        get() {
+            print("get方法")
+            return field
+        }
+        set(value) {
+            print("set方法")
+            field = value
+        }
+}
 
+interface Child {
+    fun play()
+}
+
+interface Parent {
+    fun eat()
+}
+
+class People : Child, Parent {
+    override fun play() {
+    }
+
+    override fun eat() {
+    }
+
+    var d = FF.ffv
+}
+
+class People1(child: Child, parent: Parent) : Child by child, Parent by parent
+
+object FF {
+    val ffv: String = "ff"
+}
+
+interface OnClickListener {
+    fun onClick()
+}
+
+class View {
+    var onclick: OnClickListener? = null
+}
